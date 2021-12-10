adf1c109e269e242eaa6767bfe8880a677ddddd9
KamilSkorski <95830158+KamilSkorski@users.noreply.github.com>
2021-12-10 19:58:30 +0100

    Add files via upload

---
+++ Lab1Zadanie2/Lab1Zadanie2/Lab1Zadanie2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int liczba;
+int main()
+{
+    cout << "Podaj liczbe: ", cin >> liczba;
+    if (liczba % 2 == 0)
+        cout << "Liczba jest parzysta.";
+    else cout << "Liczba jest nieparzysta.";
+}
+
